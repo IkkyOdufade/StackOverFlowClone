0b3f0acb3403b7151e2c7e91df47b928b6e5264c
Iqmah Odufade <bodufade@gmail.com>
2020-08-01 10:51:08 +0100

    added a git ignore file

--- app/controllers/answers.js
+++ app/controllers/answers.js
@@ -1,13 +1,14 @@
 const answerSchema = require('../models/answers.model');
 const question = require('../models/question.model');
+const answer = require('../models/answers.model');
 
 const AddAnswer = async (req, res) => {
 const answer = req.body;
 try {
    const newAnswer = new answerSchema(answer);
    ///check if questions exist
-   const questions =await question.findById( answer.answers.questionId )
-    if (!questions) {
+   const questionID =await question.find( answer.questionId )
+    if (!questionID) {
         ///question doesnt exist
         return res.status(403).json({
             error:true,
@@ -27,6 +28,34 @@ catch (e) {
     })
 }
 }
+const AllAnswers = async(req, res) =>{
+    try{
+        const data = await answer.find().populate('allAnswers');
+        console.log("Getting questions")
+        res.send(data);
+        client.setex("/getall", 3600, JSON.stringify(data));
+
+    }
+    catch (e) {
+        res.status(500)
+        console.log(e, "Couldn't  get all questions")
+    }
+
+};
+//function for caching all the questions from the database
+const cache = (req, res, next) => {
+    client.get("/getall", (err, data)=> {
+        if (err) {
+            return err;
+        } else if (data !== null) {
+            res.send(data);
+
+        } else {
+            next();
+        }
+    })
+};
+
 
 module.exports = {
     AddAnswer
